8f662fdd05b05b53ad58f67a3c29b3bd6ca59307
Dimas Yudha Pratama <official@dimasyudha.tech>
2023-07-23 04:35:20 +0000

    sirius: Define needed OrangeFox flags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,4 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_sirius-eng
+if $OFOX;
+    then
+        export LC_ALL='C'
+        export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+        export FOX_VERSION=R11.1_1
+        export FOX_VARIANT=A12
+        export OF_MAINTAINER='dimasyudha'
+fi
