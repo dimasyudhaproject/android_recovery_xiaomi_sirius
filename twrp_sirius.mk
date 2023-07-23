7185fde87b669c396820d7a5e7cd55914e146937
Dimas Yudha Pratama <official@dimasyudha.tech>
2023-07-23 04:36:14 +0000

    sirius: Capitalize the first letter of the word Xiaomi

--- twrp_sirius.mk
+++ twrp_sirius.mk
@@ -16,14 +16,14 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_DEVICE := sirius
-PRODUCT_NAME := twrp_sirius
+PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8 SE
-PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sirius-user 10 QKQ1.190828.002 V12.5.1.0.QEBCNXM release-keys"
+    PRIVATE_BUILD_DESC="$(PRODUCT_DEVICE)-user 10 QKQ1.190828.002 V12.5.1.0.QEBCNXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/sirius/sirius:10/QKQ1.190828.002/V12.5.1.0.QEBCNXM:user/release-keys
+BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(PRODUCT_DEVICE)/$(PRODUCT_DEVICE):10/QKQ1.190828.002/V12.5.1.0.QEBCNXM:user/release-keys
