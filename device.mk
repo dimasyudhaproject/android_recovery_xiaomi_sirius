7a635da70cb2e99a09be3c6b9ccae5b7944d0683
Dimas Yudha Pratama <official@dimasyudha.tech>
2023-07-23 04:36:30 +0000

    sirius: Build libion from source

--- device.mk
+++ device.mk
@@ -9,3 +9,10 @@
 PRODUCT_PACKAGES_ENG += \
     qcom_decrypt \
     qcom_decrypt_fbe
+
+# Recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
